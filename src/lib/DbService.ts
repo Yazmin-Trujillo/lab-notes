db8f82b07728f2cff807dc0ecdcb29a99ba9584b
Yazmin-Trujillo <lirio280688@hotmail.com>
2022-04-26 18:28:57 -0500

    refactorizar:  se cambió el subjet por un new observable

--- src/lib/DbService.ts
+++ src/lib/DbService.ts
@@ -1,6 +1,5 @@
-import { timeStamp } from "console";
-import { getFirestore, collection, addDoc, onSnapshot, Timestamp, query, orderBy, limit } from "firebase/firestore";
-import { Observable, Subject } from "rxjs";
+import { getFirestore, collection, addDoc, onSnapshot, Timestamp, query, orderBy, QuerySnapshot, DocumentData } from "firebase/firestore";
+import { Observable } from "rxjs";
 import { MyUser } from "../models/MyUser";
 import { Note } from "../models/Note";
 import { app } from "./AuthService";
@@ -19,24 +18,19 @@ export const saveNote = async (user: MyUser, note: Note) => {
         console.error("Error adding document: ", e);
     }
 };
-const noteChangedSubject = new Subject<Note[]>();
 
 export const seeNotes = (user: MyUser) => {
     const notesRef = collection(db, 'users', user.uid, "notes");
     const orderByCreateDateQuery = query(notesRef, orderBy("createDate", "desc"));
 
-    const unsub = onSnapshot(orderByCreateDateQuery, (snapshot) => {
-        let notes: Note[] = [];
-        snapshot.docs.forEach((doc) => {
-            // console.log('docdata', doc.data());
-            let note: Note = { title: doc.data().title, content: doc.data().content };
-            notes.push(note);
+    return new Observable<Note[]>(observer => {
+        onSnapshot(orderByCreateDateQuery, (snapshot) => {
+            let notes: Note[] = [];
+            snapshot.docs.forEach((doc) => {
+                let note: Note = { title: doc.data().title, content: doc.data().content };
+                notes.push(note);
+            });
+            observer.next(notes);
         });
-        noteChangedSubject.next(notes);
-        // console.log('unsub', unsub);
-        // console.log("Current data: ", snapshot.docs);
-        // snapshot.docs.forEach(doc => console.log(doc.data()))
     });
-
-    return noteChangedSubject.asObservable();
 };
